14e2d4909dd5cac3b508dad8a1de47962e9037cb
krystal2022 <sjk5689612@gmail.com>
2022-04-26 23:07:00 +0900

    파일경로변경

--- dist/main.fb6bbcaf.js
+++ dist/main.fb6bbcaf.js
@@ -133,37 +133,36 @@ var inputEls = document.querySelectorAll('input');
 var headerRightHost = document.querySelector('.header_right_host a');
 var headerRightLang = document.querySelector('.header_right_host .material-icons');
 var signIn = document.querySelector('.signin');
-var signInTxt = document.querySelector('.signinTxt'); // // 헤더가 좁아졌을 때 검색 시작하기 클릭시 헤더 넓어짐
-// for (let i = 0; i < submenuliElsBox.length; i++) {
-//   submenuliElsBox[i].addEventListener('click', function () {
-//     headerWrap.classList.remove('up')
-//     headerWrap.style.backgroundColor = "#fff"
-//     headerRightHost.style.color = "#222"
-//     headerRightLang.style.color = "#222"
-//     //i가 3개
-//     for (let i = 0; i < menunameEl.length; i++) {
-//       menunameEl[i].style.display = ""
-//       menunameEl[i].style.color = "#222"
-//       menunameEl[2].firstChild.style.color = "#222"
-//     }
-//     //i가 2개
-//     for (let i = 0; i < submenuliElsBox.length; i++) {
-//       headerLogo[0].style.cssText = "display:none"
-//       headerLogo[1].style.cssText = "display:block"
-//       startSearchTxt[i].style.cssText = "display:none"
-//       submenuEl[i].style.top = ""
-//       submenuliElsBox[i].style.cssText = ""
-//       searchEl[i].style.cssText = ""
-//     }
-//     //i가 6개
-//     for (let i = 0; i < submenuliEls.length; i++) {
-//       setTimeout(() => {
-//         submenuliEls[i].style.display = "block"
-//       }, 100)
-//     }
-//   })
-// }
-//스크롤시 헤더 변경
+var signInTxt = document.querySelector('.signinTxt'); //서브메뉴 클릭시 입체적으로 그림자생성 (디폴트)
+
+var _loop = function _loop(i) {
+  submenuliEls[i].addEventListener('click', function () {
+    for (var j = 0; j < submenuliEls.length; j++) {
+      submenuliEls[j].classList.remove('on');
+      optionMenu[j].style.display = "none";
+    }
+
+    submenuliEls[i].classList.add('on');
+    optionMenu[i].style.display = "block";
+  });
+};
+
+for (var i = 0; i < submenuliEls.length; i++) {
+  _loop(i);
+} //서브메뉴 클릭시 검색아이콘 활성화
+
+
+var _loop2 = function _loop2(_i) {
+  submenuliElsBox[_i].addEventListener('click', function () {
+    searchElTxt[_i].style.cssText = "display:block";
+    searchEl[_i].style.width = "90px";
+  });
+};
+
+for (var _i = 0; _i < searchEl.length; _i++) {
+  _loop2(_i);
+} //스크롤시 헤더 변경
+
 
 document.addEventListener('scroll', function () {
   var nowScrollTop = document.documentElement.scrollTop;
@@ -175,89 +174,60 @@ document.addEventListener('scroll', function () {
     headerRightHost.style.color = "#222";
     headerRightLang.style.color = "#222"; //i가 3개
 
-    for (var i = 0; i < menunameEl.length; i++) {
-      menunameEl[i].style.display = "none";
+    for (var _i2 = 0; _i2 < menunameEl.length; _i2++) {
+      menunameEl[_i2].style.display = "none";
     } //i가 2개
 
 
-    for (var _i = 0; _i < submenuliElsBox.length; _i++) {
+    for (var _i3 = 0; _i3 < submenuliElsBox.length; _i3++) {
       headerLogo[1].style.cssText = "display:block";
       headerLogo[0].style.cssText = "display:none";
-      startSearchTxt[_i].style.cssText = "display:block";
-      submenuEl[_i].style.top = "10px";
-      submenuliElsBox[_i].style.cssText = "width:28%; height:80%; border: 1px solid #ccc";
-      searchEl[_i].style.cssText = "width:35px; height:35px; right:10px";
-      searchElTxt[_i].style.cssText = "display:none";
+      startSearchTxt[_i3].style.cssText = "display:block";
+      submenuEl[_i3].style.top = "10px";
+      submenuliElsBox[_i3].style.cssText = "width:28%; height:80%; border: 1px solid #ccc";
+      searchEl[_i3].style.cssText = "width:35px; height:35px; right:10px";
+      searchElTxt[_i3].style.cssText = "display:none";
     } //i가 6개
 
 
-    for (var _i2 = 0; _i2 < submenuliEls.length; _i2++) {
-      submenuliEls[_i2].style.cssText = "display:none";
-      optionMenu[_i2].style.display = "none";
-    } // 헤더가 좁아졌을 때 검색 시작하기 클릭시 헤더 넓어짐
+    for (var _i4 = 0; _i4 < submenuliEls.length; _i4++) {
+      submenuliEls[_i4].style.cssText = "display:none";
+      optionMenu[_i4].style.display = "none";
+    } // 헤더가 좁아졌을 때 검색 시작하기 클릭시 헤더 넓어짐(배경흰색일 때)
 
 
-    for (var _i3 = 0; _i3 < submenuliElsBox.length; _i3++) {
-      submenuliElsBox[_i3].addEventListener('click', function () {
+    for (var _i5 = 0; _i5 < submenuliElsBox.length; _i5++) {
+      submenuliElsBox[_i5].addEventListener('click', function () {
         headerWrap.classList.remove('up');
         headerWrap.style.backgroundColor = "#fff";
         headerRightHost.style.color = "#222";
         headerRightLang.style.color = "#222"; //i가 3개
 
-        for (var _i4 = 0; _i4 < menunameEl.length; _i4++) {
-          menunameEl[_i4].style.display = "";
-          menunameEl[_i4].style.color = "#222";
-          menunameEl[2].firstChild.style.color = "#222";
+        for (var _i6 = 0; _i6 < menunameEl.length; _i6++) {
+          menunameEl[0].style.cssText = ""; // menunameEl[i].style.color = "#222"
         } //i가 2개
 
 
-        for (var _i5 = 0; _i5 < submenuliElsBox.length; _i5++) {
+        for (var _i7 = 0; _i7 < submenuliElsBox.length; _i7++) {
           headerLogo[0].style.cssText = "display:none";
           headerLogo[1].style.cssText = "display:block";
-          startSearchTxt[_i5].style.cssText = "display:none";
-          submenuEl[_i5].style.top = "";
-          submenuliElsBox[_i5].style.cssText = "";
-          searchEl[_i5].style.cssText = "";
+          startSearchTxt[_i7].style.cssText = "display:none";
+          submenuEl[_i7].style.top = "";
+          submenuliElsBox[_i7].style.cssText = "";
+          searchEl[_i7].style.cssText = "";
         } //i가 6개
 
 
-        var _loop = function _loop(_i6) {
+        var _loop3 = function _loop3(_i8) {
           setTimeout(function () {
-            submenuliEls[_i6].style.display = "block";
+            submenuliEls[_i8].style.display = "block";
           }, 100);
         };
 
-        for (var _i6 = 0; _i6 < submenuliEls.length; _i6++) {
-          _loop(_i6);
+        for (var _i8 = 0; _i8 < submenuliEls.length; _i8++) {
+          _loop3(_i8);
         }
       });
-    } //서브메뉴 클릭시 입체적으로 그림자생성 
-
-
-    var _loop2 = function _loop2(_i7) {
-      submenuliEls[_i7].addEventListener('click', function () {
-        for (var j = 0; j < submenuliEls.length; j++) {
-          submenuliEls[j].classList.remove('on');
-          optionMenu[j].style.display = "none";
-        }
-
-        submenuliEls[_i7].classList.add('on');
-
-        optionMenu[_i7].style.display = "block"; //검색아이콘 활성화
-
-        for (var _i8 = 0; _i8 < searchEl.length; _i8++) {
-          searchElTxt[_i8].style.cssText = "display:block";
-          searchEl[_i8].style.width = "90px"; // if (i === 0) {
-          //   submenuliEls[0].style.cssText = "padding-right:100px";
-          // } else if (i === 1) {
-          //   submenuliEls[4].style.cssText = "padding-right:10px";
-          // }
-        }
-      });
-    };
-
-    for (var _i7 = 0; _i7 < submenuliEls.length; _i7++) {
-      _loop2(_i7);
     } //헤더가 넓어졌을 때 (스크롤이 0일 때)
 
   } else if (nowScrollTop === 0) {
@@ -283,79 +253,109 @@ document.addEventListener('scroll', function () {
     } //i가 6개
 
 
-    var _loop3 = function _loop3(_i11) {
+    var _loop4 = function _loop4(_i11) {
       setTimeout(function () {
         submenuliEls[_i11].style.display = "block";
       }, 100);
     };
 
     for (var _i11 = 0; _i11 < submenuliEls.length; _i11++) {
-      _loop3(_i11);
+      _loop4(_i11);
     } // 헤더가 넓어졌을 때(스크롤 0일때) 검색 시작하기 클릭시 옵션창뜨기(헤더모양 그대로 유지)
 
 
     for (var _i12 = 0; _i12 < submenuliElsBox.length; _i12++) {
+      for (var _i13 = 0; _i13 < menunameEl.length; _i13++) {
+        menunameEl[_i13].style.cssText = "";
+        menunameEl[2].firstChild.style.cssText = "";
+      }
+
       submenuliElsBox[_i12].addEventListener('click', function () {
         headerWrap.classList.remove('up');
         headerWrap.style.backgroundColor = "";
         headerRightHost.style.color = "#fff";
         headerRightLang.style.color = "#fff"; //i가 3개
 
-        for (var _i13 = 0; _i13 < menunameEl.length; _i13++) {
-          menunameEl[_i13].style.display = "";
-          menunameEl[_i13].style.color = "#fff";
-          menunameEl[2].firstChild.style.color = "#fff";
+        for (var _i14 = 0; _i14 < menunameEl.length; _i14++) {
+          menunameEl[_i14].style.cssText = "";
+          menunameEl[2].firstChild.style.cssText = "";
         } //i가 2개
 
 
-        for (var _i14 = 0; _i14 < submenuliElsBox.length; _i14++) {
+        for (var _i15 = 0; _i15 < submenuliElsBox.length; _i15++) {
           headerLogo[0].style.cssText = "display:block";
           headerLogo[1].style.cssText = "display:none";
-          startSearchTxt[_i14].style.cssText = "display:none";
-          submenuEl[_i14].style.top = "";
-          submenuliElsBox[_i14].style.cssText = "";
-          searchEl[_i14].style.cssText = "";
+          startSearchTxt[_i15].style.cssText = "display:none";
+          submenuEl[_i15].style.top = "";
+          submenuliElsBox[_i15].style.cssText = ""; // searchEl[i].style.cssText = ""
         } //i가 6개
 
 
-        var _loop4 = function _loop4(_i15) {
+        var _loop5 = function _loop5(_i16) {
           setTimeout(function () {
-            submenuliEls[_i15].style.display = "block";
+            submenuliEls[_i16].style.display = "block";
           }, 100);
         };
 
-        for (var _i15 = 0; _i15 < submenuliEls.length; _i15++) {
-          _loop4(_i15);
+        for (var _i16 = 0; _i16 < submenuliEls.length; _i16++) {
+          _loop5(_i16);
         }
       });
+    }
+  } //서브메뉴 클릭시 검색아이콘 활성화
+
+
+  var _loop6 = function _loop6(_i17) {
+    submenuliElsBox[_i17].addEventListener('click', function () {
+      searchElTxt[_i17].style.cssText = "display:block";
+      searchEl[_i17].style.width = "90px";
+    });
+  };
+
+  for (var _i17 = 0; _i17 < searchEl.length; _i17++) {
+    _loop6(_i17);
+  } //스크롤한 후 클릭했던 서브메뉴와 옵션창이 다른 서브메뉴를 클릭하면 리셋되도록(클릭안한 상태로 돌아가도록)
+
+
+  for (var _i18 = 0; _i18 < submenuliEls.length; _i18++) {
+    submenuliEls[_i18].classList.remove('on');
+  }
+
+  var headerColor = headerWrap.css.backgroundColor;
+
+  if (headerColor === '#fff') {
+    for (var _i19 = 0; menunameEl.length; _i19++) {
+      menunameEl[_i19].classList.add('black');
     }
   }
 }); // 메인메뉴 클릭시 서브옵션메뉴 생성
 
-var _loop5 = function _loop5(i) {
-  menunameEl[i].addEventListener('click', function () {
+var _loop7 = function _loop7(_i20) {
+  menunameEl[_i20].addEventListener('click', function () {
     for (var j = 0; j < 2; j++) {
       submenuEl[j].classList.add('hide');
       menunameEl[j].classList.remove('on');
     }
 
-    submenuEl[i].classList.remove('hide');
-    menunameEl[i].classList.add('on');
-    searchElTxt[i].style.cssText = "display:none";
-    searchEl[i].style.width = "";
+    submenuEl[_i20].classList.remove('hide');
 
-    if (i === 0) {
+    menunameEl[_i20].classList.add('on');
+
+    searchElTxt[_i20].style.cssText = "display:none";
+    searchEl[_i20].style.width = "";
+
+    if (_i20 === 0) {
       optionList[1].style.display = "none";
       optionList[0].style.display = "block";
-    } else if (i === 1) {
+    } else if (_i20 === 1) {
       optionList[0].style.display = "none";
       optionList[1].style.display = "block";
     }
   });
 };
 
-for (var i = 0; i < 2; i++) {
-  _loop5(i);
+for (var _i20 = 0; _i20 < 2; _i20++) {
+  _loop7(_i20);
 } //signin 버튼 클릭시 옵션창(회원가입/로그인 등) 활성화
 
 
@@ -364,55 +364,50 @@ signIn.addEventListener('click', function () {
 });
 signIn.addEventListener('blur', function () {
   signInTxt.style.display = "";
-}); // 검색아이콘 클릭 시 검색문구 추가 / 첫번째 서브메뉴 너비 줄어듬 (숙소메뉴0 / 체험메뉴1)
+}); // 검색아이콘 클릭 시 검색문구 아이콘에 추가 / 첫번째 옵션창 활성화
 
-var _loop6 = function _loop6(_i16) {
+var _loop8 = function _loop8(_i21) {
   // 검색 아이콘 클릭
-  searchEl[_i16].addEventListener('click', function () {
-    searchElTxt[_i16].style.cssText = "display:block";
-    searchEl[_i16].style.width = "90px"; //숙소메뉴 검색아이콘0 클릭시
+  searchEl[_i21].addEventListener('click', function () {
+    searchElTxt[_i21].style.cssText = "display:block";
+    searchEl[_i21].style.width = "90px"; //숙소메뉴 검색아이콘0 클릭시
 
-    if (_i16 === 0) {
+    if (_i21 === 0) {
       optionMenu[0].style.display = "block";
-      submenuliEls[0].classList.add('on');
 
       for (var j = 1; j < 4; j++) {
         optionMenu[j].style.display = "none";
-        submenuliEls[j].classList.remove('on');
       } //체험메뉴 검색아이콘1 클릭시  
 
-    } else if (_i16 === 1) {
+    } else if (_i21 === 1) {
       optionMenu[4].style.display = "block";
-      submenuliEls[4].classList.add('on');
 
       for (var _j = 5; _j < 6; _j++) {
         optionMenu[_j].style.display = "none";
-
-        submenuliEls[_j].classList.remove('on');
       }
     }
   }); //옵션창 활성화 상태에서 다른 부분 클릭시 옵션창 비활성화
 
 
-  submenuEl[_i16].addEventListener('click', function () {
-    optionList[_i16].style.display = "block";
+  submenuEl[_i21].addEventListener('click', function () {
+    optionList[_i21].style.display = "block";
   });
 
-  submenuEl[_i16].addEventListener('blur', function () {
-    optionList[_i16].style.display = "none";
+  submenuEl[_i21].addEventListener('blur', function () {
+    optionList[_i21].style.display = "none";
 
     for (var j = 0; j < submenuliEls.length; j++) {
       submenuliEls[j].classList.remove('on');
     }
 
-    searchElTxt[_i16].style.cssText = "display:none";
-    searchEl[_i16].style.width = "";
+    searchElTxt[_i21].style.cssText = "display:none";
+    searchEl[_i21].style.width = "";
   }); // 옵션창 내의 input 클릭 후 다른 부분 클릭시 옵션창 비활성화
 
 
-  inputEls[_i16].addEventListener('blur', function () {
-    optionList[_i16].style.display = "none";
-    inputEls[_i16].value = "";
+  inputEls[_i21].addEventListener('blur', function () {
+    optionList[_i21].style.display = "none";
+    inputEls[_i21].value = "";
 
     for (var j = 0; j < submenuliEls.length; j++) {
       submenuliEls[j].classList.remove('on');
@@ -420,8 +415,8 @@ var _loop6 = function _loop6(_i16) {
   });
 };
 
-for (var _i16 = 0; _i16 < searchEl.length; _i16++) {
-  _loop6(_i16);
+for (var _i21 = 0; _i21 < searchEl.length; _i21++) {
+  _loop8(_i21);
 } // //서브메뉴 클릭시 입체적으로 그림자생성 
 // for (let i = 0; i < submenuliEls.length; i++) {
 //   submenuliEls[i].addEventListener('click', function () {
@@ -431,16 +426,14 @@ for (var _i16 = 0; _i16 < searchEl.length; _i16++) {
 //     }
 //     submenuliEls[i].classList.add('on')
 //     optionMenu[i].style.display = "block"
-//     //검색아이콘 활성화
-//     for (let i = 0; i < searchEl.length; i++) {
-//       searchElTxt[i].style.cssText = "display:block";
-//       searchEl[i].style.width = "90px"
-//       // if (i === 0) {
-//       //   submenuliEls[0].style.cssText = "padding-right:100px";
-//       // } else if (i === 1) {
-//       //   submenuliEls[4].style.cssText = "padding-right:10px";
-//       // }
-//     }
+//   })
+// }
+// //서브메뉴 클릭시 검색아이콘 활성화
+// for (let i = 0; i < searchEl.length; i++) {
+//   submenuliElsBox[i].addEventListener('click', function () {
+//     searchElTxt[i].style.cssText = "display:block";
+//     searchEl[i].style.width = "90px"
+//     console.log(searchEl)
 //   })
 // }
 // 인원추가 버튼
@@ -452,54 +445,54 @@ var plusiconLine = document.getElementsByClassName('button__plus');
 var plusicon = document.querySelectorAll('.button__plus span');
 var resultEl = document.getElementsByClassName('result');
 
-var _loop7 = function _loop7(_i17) {
-  minusicon[_i17].addEventListener('click', function () {
-    var number = resultEl[_i17].innerText;
+var _loop9 = function _loop9(_i22) {
+  minusicon[_i22].addEventListener('click', function () {
+    var number = resultEl[_i22].innerText;
     number = parseInt(number) - 1;
 
     if (number < 0) {
       number = 0;
     } else if (0 < number < 5) {
-      plusiconLine[_i17].style.borderColor = "grey";
-      plusicon[_i17].style.color = "grey";
-      minusiconLine[_i17].style.borderColor = "grey";
-      minusicon[_i17].style.color = "grey";
+      plusiconLine[_i22].style.borderColor = "grey";
+      plusicon[_i22].style.color = "grey";
+      minusiconLine[_i22].style.borderColor = "grey";
+      minusicon[_i22].style.color = "grey";
     }
 
     if (number === 0) {
-      minusiconLine[_i17].style.borderColor = "#ccc";
-      minusicon[_i17].style.color = "#ccc";
-      plusiconLine[_i17].style.borderColor = "grey";
-      plusicon[_i17].style.color = "grey";
+      minusiconLine[_i22].style.borderColor = "#ccc";
+      minusicon[_i22].style.color = "#ccc";
+      plusiconLine[_i22].style.borderColor = "grey";
+      plusicon[_i22].style.color = "grey";
     }
 
-    resultEl[_i17].innerText = number;
+    resultEl[_i22].innerText = number;
   });
 
-  plusicon[_i17].addEventListener('click', function () {
-    var number = resultEl[_i17].innerText;
+  plusicon[_i22].addEventListener('click', function () {
+    var number = resultEl[_i22].innerText;
     number = parseInt(number) + 1;
 
     if (number > 5) {
       number = 5;
     } else if (0 < number < 5) {
-      minusiconLine[_i17].style.borderColor = "grey";
-      minusicon[_i17].style.color = "grey";
+      minusiconLine[_i22].style.borderColor = "grey";
+      minusicon[_i22].style.color = "grey";
     }
 
     if (number === 5) {
-      plusiconLine[_i17].style.borderColor = "#ccc";
-      plusicon[_i17].style.color = "#ccc";
-      minusiconLine[_i17].style.borderColor = "grey";
-      minusicon[_i17].style.color = "grey";
+      plusiconLine[_i22].style.borderColor = "#ccc";
+      plusicon[_i22].style.color = "#ccc";
+      minusiconLine[_i22].style.borderColor = "grey";
+      minusicon[_i22].style.color = "grey";
     }
 
-    resultEl[_i17].innerText = number;
+    resultEl[_i22].innerText = number;
   });
 };
 
-for (var _i17 = 0; _i17 < minusicon.length; _i17++) {
-  _loop7(_i17);
+for (var _i22 = 0; _i22 < minusicon.length; _i22++) {
+  _loop9(_i22);
 }
 },{}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
@@ -529,7 +522,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51784" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53920" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
